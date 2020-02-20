08c64dc5fbddcde347beb42e7b034e8396080c66
Rinaldi00 <61256282+Rinaldi00@users.noreply.github.com>
2020-02-20 01:00:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ module.exports = {
     token: "oauth:6rteu52sfk9ifv7qr917mkc58l2xld",
     channels: [
         "pepo3393live",
+        "kafkanya",
+        "caku21",
+        "pio3d",
+        "superimen",
     ]
 }
